31dd9c404d5dd93da8350b7908eda294acd241a0
Swiip <matthieu.lux@gmail.com>
2017-04-17 11:02:58 +0200

    initial commit from citation/citation-demo

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Component1 from './Component1';
+import Component2 from './Component2';
+import Component3 from './Component3';
+import ComponentList from './common/ComponentList';
+import PageOrComponent from './common/PageOrComponent';
+import Banner from './home/Banner';
+import Home from './home/Home';
+import TopMenu from './home/TopMenu';
+
+export default {
+	Component1,
+	Component2,
+	Component3,
+	ComponentList,
+	PageOrComponent,
+	TopMenu,
+	Banner,
+	Home
+};
